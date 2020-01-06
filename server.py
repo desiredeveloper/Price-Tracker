e9e893828103bfcc3329dc6080e1b9ac9ab49fa6
shreyansh <sjain0615@gmail.com>
2020-01-06 11:58:13 +0530

    code refactored and cron added

--- server.py
+++ server.py
@@ -9,12 +9,15 @@ import argparse
 import json
 import logging
 import sys
+import re
 import requests
-from functools import wraps
-import numpy as np
 from flask import Flask, Response, jsonify, request
 from flask_mail import Mail, Message
-from scraper import Scraper
+from subscriber import Subscriber
+import atexit
+from bs4 import BeautifulSoup 
+from datetime import date,datetime,timedelta
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 parser = argparse.ArgumentParser()
 
@@ -23,11 +26,19 @@ parser.add_argument(
 parser.add_argument(
     "--port", default=8500, help="The port of the server(eg. 8500)", type=int)
 parser.add_argument(
+	"--classId", default="_1vC4OE _3qQ9m1", help="Class for scrapping")
+parser.add_argument(
     "--debug",
     default=False,
     help="Enable debug for flask or not(eg. False)",
     type=bool)
+parser.add_argument(
+	"--interval", 
+	default=60, 
+	help="Time interval between each notification(eg. 15)", 
+	type=int)
 
+args = parser.parse_args(sys.argv[1:])
 application = Flask(__name__)
 application.config.update(
 	DEBUG=True,
@@ -36,30 +47,69 @@ application.config.update(
 	MAIL_PORT=465,
 	MAIL_USE_SSL=True,
 	MAIL_USERNAME = 'it1402713094@gmail.com',
-	MAIL_PASSWORD = 'yourpassword'
+	MAIL_PASSWORD = 'flaskapp'
 	)
 mail = Mail(application)
 
-def send_mail(msg):
+
+cron = BlockingScheduler()
+cron.start()
+
+@cron.scheduled_job('interval',id='my_job_id',seconds=args.interval)
+def send_mail():
 	try:
-		msgObj = Message("Send Mail Tutorial!",
-		  sender="it1402713094@gmail.com",
-		  recipients=["recievingemail@email.com"])
-		msgObj.body = msg
-		mail.send(msgObj)
+		with open('tracker.json', 'r') as f:
+			trackData = json.load(f)
+
+			today = date.today()
+			yesterday = str(today-timedelta(days=1))
+			str_today = str(today)
+
+			for pid in trackData:
+				msg = 0
+				r = requests.get(trackData[pid]['URL']) 
+				soup = BeautifulSoup(r.content, 'html5lib')
+
+				price = soup.find('div', attrs = {'class':args.classId}).text
+				price = re.sub("[\D]","",price)
+
+				x = datetime.now()
+
+				if trackData[pid].get(str_today) is None:
+					trackData[pid][str_today] = [{"min":str(float("inf"))}]
+
+				trackData[pid][str_today].append({x:price})
+
+				trackData[pid][str_today][0]['min'] = min(trackData[pid][str_today][0]['min'], price)
+
+				if(trackData[pid][yesterday][0]["min"] > trackData[pid][str_today][0]["min"]):
+					msg = "Better price than yesterday for \n" + trackData[pid]['URL']+ " The price now is Rs."+price
+
+				if trackData[pid]['global_min']>price:
+					trackData[pid]['global_min'] = price
+					msg = "Lowest Price ever for \n"+ trackData[pid]['URL'] +"\n\n Available @ Rs."+ price
+				
+				if msg!=0:
+					recv_list = trackData[pid]['mailing_list']
+					msgObj = Message("Update on product-ID :"+pid,
+					sender="it1402713094@gmail.com",
+					recipients= recv_list)
+					msgObj.body = msg
+					mail.send(msgObj)
 	except Exception as e:
-		return(str(e)) 
+		return(str(e))
+
+# Shutdown your cron thread if the web process is stopped
+atexit.register(lambda: cron.shutdown(wait=False))
 
 
 @application.route("/", methods=["POST"])
 def subscribe():
     requestData = request.json
-    product = Scraper(requestData.url,requestData.email)
-    msgId = product.tracker()
-
+    product = Subscriber(requestData.url,requestData.email)
+    product.subscribe()
 
 def main():
-    args = parser.parse_args(sys.argv[1:])
     application.run(host=args.host, port=args.port, debug=args.debug)
 
 
